05b42171b2c21d9d22cff3bae0b1cd9c128981fa
Jaden Geller <jaden.geller@gmail.com>
2015-11-25 21:51:04 -0800

    Initial Commit

---
+++ Unity/Unity.h
@@ -0,0 +1,19 @@
+//
+//  Unity.h
+//  Unity
+//
+//  Created by Jaden Geller on 11/25/15.
+//  Copyright © 2015 Jaden Geller. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Unity.
+FOUNDATION_EXPORT double UnityVersionNumber;
+
+//! Project version string for Unity.
+FOUNDATION_EXPORT const unsigned char UnityVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Unity/PublicHeader.h>
+
+
