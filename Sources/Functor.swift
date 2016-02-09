c645b71a78c3a20d399931b7ff9d394a5ce059b5
Jaden Geller <jaden.geller@gmail.com>
2016-02-09 09:24:07 -0800

    Documented

--- Sources/Functor.swift
+++ Sources/Functor.swift
@@ -7,22 +7,27 @@
 //
 
 extension Term {
+    /// The name-arity signature of `self`.
     public var functor: Functor<Atom> {
         return Functor(name: name, arity: arity)
     }
 }
 
+/// The name-arity signature of a `Term`. Defines a class of `Term`s that can potentially
+/// be unified with each other.
 public struct Functor<Atom: Hashable> {
     public let name: Atom
     public let arity: Int
 }
 
 extension Functor: Hashable {
+    /// The hash value.
     public var hashValue: Int {
         return name.hashValue ^ arity.hashValue
     }
 }
 
+/// Returns `true` if `lhs` has the same name and arity as `rhs`.
 public func ==<Atom: Hashable>(lhs: Functor<Atom>, rhs: Functor<Atom>) -> Bool {
     return lhs.name == rhs.name && lhs.arity == rhs.arity
 }
--- Sources/System.swift
+++ Sources/System.swift
@@ -6,15 +6,22 @@
 //  Copyright © 2016 Jaden Geller. All rights reserved.
 //
 
+/// Set to `true` to enable trace mode.
+private let DEBUG = false
+
 import Gluey
 
+/// A logic `System`, defined by a collection of `Clauses`, that provides a mechanism for querying
+/// to learn facts about the system.
 public struct System<Atom: Hashable> {
     private let clauses: [Functor<Atom> : [Clause<Atom>]]
     
+    /// Constructs a `System` from a sequence of `Clause`s.
     public init<S: SequenceType where S.Generator.Element == Clause<Atom>>(clauses: S) {
         self.clauses = clauses.groupBy{ $0.head.functor }
     }
     
+    /// Returns unique copies of the clauses that might bind with a `Term` of a given `functor`.
     private func uniqueClausesWithFunctor(functor: Functor<Atom>) -> LazyMapSequence<[Clause<Atom>], Clause<Atom>> {
         let nonUniqueClauses = clauses[functor] ?? []
         let context = CopyContext()
@@ -23,30 +30,45 @@ public struct System<Atom: Hashable> {
 }
 
 extension System {
+    /// Attempts to unify each term in `goals` with the known clauses in the system, calling `onMatch` each
+    /// time it succeeds to simultaneously unify all `goals`.
     public func enumerateMatches(goals: [Term<Atom>], onMatch: () throws -> ()) throws {
         // Reverse first since reduce is right-to-left
         let satisfyAll = goals.reverse().reduce(onMatch) { lambda, predicate in { try self.enumerateMatches(predicate, onMatch: lambda) } }
         try satisfyAll()
     }
     
+    /// Attempts to unify `goal` with the known clauses in the system calling `onMatch` each time it succeeds to unify.
     public func enumerateMatches(goal: Term<Atom>, onMatch: () throws -> ()) throws {
-        print("GOAL: \(goal)")
+        #if debug
+            print("GOAL: \(goal)")
+        #endif
         for clause in uniqueClausesWithFunctor(goal.functor) {
+            #if debug
             print("ATTEMPT: \(clause)")
+            #endif
             do {
                 try Term.attempt(goal) {
                     try Term.unify(goal, clause.head)
+                    #if debug
                     print("CALL: \(clause.head)")
+                    #endif
                     try self.enumerateMatches(clause.body) {
+                        #if debug
                         print("SUCCESS: \(clause.head)")
+                        #endif
                         try onMatch()
                         throw UnificationError("CONTINUE") // We need a way to end early.
                     }
                 }
+                #if debug
                 print("DONE")
+                #endif
                 return
             } catch let error as UnificationError {
+                #if debug
                 print("BACKTRACKING: \(error)")
+                #endif
                 continue
             }
         }
