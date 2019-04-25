0d664b4747bdae3401baac23a316bb8471cc8b89
Max Desiatov <max@desiatov.com>
2019-04-25 11:36:05 +0200

    Add Axiomatic library product to Package.swift

--- Package.swift
+++ Package.swift
@@ -5,6 +5,14 @@ import PackageDescription
 
 let package = Package(
   name: "Axiomatic",
+  products: [
+    // Products define the executables and libraries produced by a package,
+    // and make them visible to other packages.
+    .library(
+      name: "Axiomatic",
+      targets: ["Axiomatic"]
+    ),
+  ],
   dependencies: [
     .package(url: "https://github.com/MaxDesiatov/Gluey.git", .branch("master")),
   ],
